29b30328ce1821807d7c980d2b87246f909960b1
Andrej Benz <hello@benz.dev>
2025-10-05 10:09:09 +0200

    feat: websearch engines_as_actions

--- internal/providers/websearch/setup.go
+++ internal/providers/websearch/setup.go
@@ -36,6 +36,7 @@ type Config struct {
 	MaxGlobalItemsToDisplay int     `koanf:"max_global_items_to_display" desc:"will only show the global websearch entry if there are at most X results." default:"1"`
 	History                 bool    `koanf:"history" desc:"make use of history for sorting" default:"true"`
 	HistoryWhenEmpty        bool    `koanf:"history_when_empty" desc:"consider history when query is empty" default:"false"`
+	EnginesAsActions        bool    `koanf:"engines_as_actions" desc:"run engines as actions" default:"true"`
 }
 
 type Entry struct {
@@ -55,6 +56,7 @@ func Setup() {
 		MaxGlobalItemsToDisplay: 1,
 		History:                 true,
 		HistoryWhenEmpty:        false,
+		EnginesAsActions:        false,
 	}
 
 	common.LoadConfig(Name, config)
@@ -107,31 +109,48 @@ func Activate(identifier, action string, query string, args string) {
 			args = query
 		}
 
-		url := strings.ReplaceAll(config.Entries[i].URL, "%TERM%", url.QueryEscape(strings.TrimSpace(args)))
-
-		prefix := common.LaunchPrefix("")
-
-		cmd := exec.Command("sh", "-c", strings.TrimSpace(fmt.Sprintf("%s xdg-open '%s'", prefix, url)))
-
-		cmd.SysProcAttr = &syscall.SysProcAttr{
-			Setsid: true,
-		}
-
-		err := cmd.Start()
-		if err != nil {
-			slog.Error(Name, "activate", err)
-		} else {
-			go func() {
-				cmd.Wait()
-			}()
-		}
-
-		if config.History {
-			h.Save(query, identifier)
-		}
+		run(query, identifier, strings.ReplaceAll(config.Entries[i].URL, "%TERM%", url.QueryEscape(strings.TrimSpace(args))))
 	default:
-		slog.Error(Name, "activate", fmt.Sprintf("unknown action: %s", action))
-		return
+		q := ""
+
+		if !config.EnginesAsActions {
+			slog.Error(Name, "activate", fmt.Sprintf("unknown action: %s", action))
+			return
+		}
+
+		for _, v := range config.Entries {
+			if v.Name == action {
+				q = v.URL
+				break
+			}
+		}
+
+		q = strings.ReplaceAll(q, "%TERM%", url.QueryEscape(strings.TrimSpace(query)))
+
+		run(query, identifier, q)
+	}
+}
+
+func run(query, identifier, q string) {
+	prefix := common.LaunchPrefix("")
+
+	cmd := exec.Command("sh", "-c", strings.TrimSpace(fmt.Sprintf("%s xdg-open '%s'", prefix, q)))
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+
+	err := cmd.Start()
+	if err != nil {
+		slog.Error(Name, "activate", err)
+	} else {
+		go func() {
+			cmd.Wait()
+		}()
+	}
+
+	if config.History {
+		h.Save(query, identifier)
 	}
 }
 
@@ -147,58 +166,27 @@ func Query(conn net.Conn, query string, single bool, exact bool) []*pb.QueryResp
 		}
 	}
 
-	if single {
-		for k, v := range config.Entries {
-			icon := v.Icon
-			if icon == "" {
-				icon = config.Icon
-			}
+	if config.EnginesAsActions {
+		a := []string{}
 
-			e := &pb.QueryResponse_Item{
-				Identifier: strconv.Itoa(k),
-				Text:       v.Name,
-				Subtext:    "",
-				Actions:    []string{"search"},
-				Icon:       icon,
-				Provider:   Name,
-				Score:      int32(100 - k),
-				Type:       0,
-			}
-
-			if query != "" {
-				score, pos, start := common.FuzzyScore(query, v.Name, exact)
-
-				e.Score = score
-				e.Fuzzyinfo = &pb.QueryResponse_Item_FuzzyInfo{
-					Field:     "text",
-					Positions: pos,
-					Start:     start,
-				}
-			}
-
-			var usageScore int32
-			if config.History {
-				if e.Score > config.MinScore || query == "" && config.HistoryWhenEmpty {
-					usageScore = h.CalcUsageScore(query, e.Identifier)
-
-					if usageScore != 0 {
-						e.State = append(e.State, "history")
-						e.Actions = append(e.Actions, history.ActionDelete)
-					}
-
-					e.Score = e.Score + usageScore
-				}
-			}
-
-			if e.Score > config.MinScore || query == "" {
-				entries = append(entries, e)
-			}
+		for _, v := range config.Entries {
+			a = append(a, v.Name)
 		}
-	}
 
-	if len(entries) == 0 || !single {
-		for k, v := range config.Entries {
-			if v.Default || v.Prefix == prefix {
+		e := &pb.QueryResponse_Item{
+			Identifier: "websearch",
+			Text:       fmt.Sprintf("Search: %s", query),
+			Actions:    a,
+			Icon:       Icon(),
+			Provider:   Name,
+			Score:      int32(100),
+			Type:       0,
+		}
+
+		entries = append(entries, e)
+	} else {
+		if single {
+			for k, v := range config.Entries {
 				icon := v.Icon
 				if icon == "" {
 					icon = config.Icon
@@ -215,7 +203,58 @@ func Query(conn net.Conn, query string, single bool, exact bool) []*pb.QueryResp
 					Type:       0,
 				}
 
-				entries = append(entries, e)
+				if query != "" {
+					score, pos, start := common.FuzzyScore(query, v.Name, exact)
+
+					e.Score = score
+					e.Fuzzyinfo = &pb.QueryResponse_Item_FuzzyInfo{
+						Field:     "text",
+						Positions: pos,
+						Start:     start,
+					}
+				}
+
+				var usageScore int32
+				if config.History {
+					if e.Score > config.MinScore || query == "" && config.HistoryWhenEmpty {
+						usageScore = h.CalcUsageScore(query, e.Identifier)
+
+						if usageScore != 0 {
+							e.State = append(e.State, "history")
+							e.Actions = append(e.Actions, history.ActionDelete)
+						}
+
+						e.Score = e.Score + usageScore
+					}
+				}
+
+				if e.Score > config.MinScore || query == "" {
+					entries = append(entries, e)
+				}
+			}
+		}
+
+		if len(entries) == 0 || !single {
+			for k, v := range config.Entries {
+				if v.Default || v.Prefix == prefix {
+					icon := v.Icon
+					if icon == "" {
+						icon = config.Icon
+					}
+
+					e := &pb.QueryResponse_Item{
+						Identifier: strconv.Itoa(k),
+						Text:       v.Name,
+						Subtext:    "",
+						Actions:    []string{"search"},
+						Icon:       icon,
+						Provider:   Name,
+						Score:      int32(100 - k),
+						Type:       0,
+					}
+
+					entries = append(entries, e)
+				}
 			}
 		}
 	}
