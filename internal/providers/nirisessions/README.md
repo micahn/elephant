237909aa9f91514329463f9096a190063c6bb644
Andrej Benz <hello@benz.dev>
2025-10-23 17:24:33 +0200

    feat: niri sessions provider

---
+++ internal/providers/nirisessions/README.md
@@ -0,0 +1,21 @@
+### Elephant Niri Sessions
+
+Create predefined session layouts and open them.
+
+#### Features
+
+- run custom commands to open windows
+- position windows according to definition
+
+#### Requirements
+
+- `niri`
+
+#### Example snippets
+
+```toml
+[[snippets]]
+keywords = ["search", "this"]
+name = "example snippet"
+content = "this will be pasted"
+```
---
+++ internal/providers/nirisessions/setup.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net"
+	"os/exec"
+	"strconv"
+	"time"
+
+	_ "embed"
+
+	"github.com/abenz1267/elephant/v2/internal/util"
+	"github.com/abenz1267/elephant/v2/pkg/common"
+	"github.com/abenz1267/elephant/v2/pkg/pb/pb"
+)
+
+var (
+	Name       = "nirisessions"
+	NamePretty = "Niri Sessions"
+	config     *Config
+)
+
+//go:embed README.md
+var readme string
+
+const (
+	ActionStart    = "start"
+	ActionStartNew = "start_new"
+)
+
+type Config struct {
+	common.Config `koanf:",squash"`
+	Sessions      []Session `koanf:"sessions" desc:"define the sessions" default:""`
+}
+
+type Session struct {
+	Name       string      `koanf:"name" desc:"name for the session" default:""`
+	Workspaces []Workspace `koanf:"workspaces" desc:"set of workspaces" default:""`
+}
+
+type Workspace struct {
+	Windows []Window `koanf:"windows" desc:"windows in this workspace group" default:""`
+}
+
+type Window struct {
+	Command string `koanf:"command" desc:"command to run" default:""`
+	AppID   string `koanf:"app_id" desc:"app_id to identify the window" default:""`
+}
+
+func Setup() {
+	config = &Config{
+		Config: common.Config{
+			Icon:     "insert-text",
+			MinScore: 20,
+		},
+	}
+
+	common.LoadConfig(Name, config)
+}
+
+func PrintDoc() {
+	fmt.Println(readme)
+	fmt.Println()
+	util.PrintConfig(Config{}, Name)
+}
+
+type OpenedOrChangedEvent struct {
+	WindowOpenedOrChanged *struct {
+		Window struct {
+			AppID  string `json:"app_id"`
+			Layout struct {
+				PosInScrollingLayout []int `json:"pos_in_scrolling_layout"`
+			} `json:"layout"`
+		} `json:"window"`
+	} `json:"WindowOpenedOrChanged,omitempty"`
+}
+
+func monitor(appid string, res chan struct{}) {
+	cmd := exec.Command("niri", "msg", "-j", "event-stream")
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		slog.Error(Name, "monitor", err)
+		return
+	}
+
+	if err := cmd.Start(); err != nil {
+		slog.Error(Name, "monitor", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(stdout)
+
+	for scanner.Scan() {
+		var e OpenedOrChangedEvent
+		err := json.Unmarshal(scanner.Bytes(), &e)
+		if err != nil {
+			slog.Error(Name, "event unmarshal", err)
+		}
+
+		if e.WindowOpenedOrChanged != nil && e.WindowOpenedOrChanged.Window.AppID == appid && e.WindowOpenedOrChanged.Window.Layout.PosInScrollingLayout != nil {
+			res <- struct{}{}
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		slog.Error(Name, "monitor", err)
+		return
+	}
+
+	if err := cmd.Wait(); err != nil {
+		slog.Error(Name, "monitor", err)
+		return
+	}
+}
+
+func Activate(identifier, action string, query string, args string) {
+	i, _ := strconv.Atoi(identifier)
+
+	s := config.Sessions[i]
+
+	res := make(chan struct{})
+
+	if action == ActionStartNew {
+		goWorkspaceDown()
+	}
+
+	for k, v := range s.Workspaces {
+		for _, w := range v.Windows {
+			go monitor(w.AppID, res)
+
+			cmd := exec.Command("sh", "-c", w.Command)
+			err := cmd.Start()
+			if err != nil {
+				slog.Error(Name, "activate", err)
+				return
+			} else {
+				go func() {
+					cmd.Wait()
+				}()
+			}
+			<-res
+		}
+
+		if k < len(s.Workspaces)-1 {
+			goWorkspaceDown()
+		}
+	}
+}
+
+func goWorkspaceDown() {
+	cmd := exec.Command("niri", "msg", "action", "focus-workspace-down")
+	err := cmd.Start()
+	if err != nil {
+		slog.Error(Name, "activate", err)
+		return
+	} else {
+		go func() {
+			cmd.Wait()
+		}()
+	}
+}
+
+func Query(conn net.Conn, query string, single bool, exact bool) []*pb.QueryResponse_Item {
+	start := time.Now()
+
+	entries := []*pb.QueryResponse_Item{}
+
+	for k, v := range config.Sessions {
+		e := &pb.QueryResponse_Item{
+			Identifier: fmt.Sprintf("%d", k),
+			Text:       v.Name,
+			Icon:       config.Icon,
+			Provider:   Name,
+			Actions:    []string{ActionStart, ActionStartNew},
+		}
+
+		if query != "" {
+			score, positions, start := common.FuzzyScore(query, v.Name, exact)
+
+			e.Score = score
+			e.Fuzzyinfo = &pb.QueryResponse_Item_FuzzyInfo{
+				Start:     start,
+				Field:     "text",
+				Positions: positions,
+			}
+		}
+
+		if query == "" || e.Score > config.MinScore {
+			entries = append(entries, e)
+		}
+	}
+
+	slog.Info(Name, "queryresult", len(entries), "time", time.Since(start))
+
+	return entries
+}
+
+func Icon() string {
+	return config.Icon
+}
