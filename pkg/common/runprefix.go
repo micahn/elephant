e7d65f96729a4df867509d470872f3f0f5612f9a
Andrej Benz <hello@benz.dev>
2025-10-28 14:02:23 +0100

    feat: detect niri for launch prefix (not enabled because of a niri bug)

--- pkg/common/runprefix.go
+++ pkg/common/runprefix.go
@@ -34,6 +34,14 @@ func InitRunPrefix() {
 		}
 	}
 
+	// TODO: enable once niri bug is fixed
+	// niri, err := exec.LookPath("niri")
+	// if err == nil && niri != "" {
+	// 	runPrefix = "niri msg action spawn-sh --"
+	// slog.Info("config", "runprefix autodetect", runPrefix)
+	// 	return
+	// }
+
 	spid := os.Getenv("SYSTEMD_EXEC_PID")
 	if spid != "" {
 		systemdrun, err := exec.LookPath("systemd-run")
