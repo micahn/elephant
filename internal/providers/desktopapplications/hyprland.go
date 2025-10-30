ee119d88758a33db18957f602e58d0d50aab8f0c
Andrej Benz <hello@benz.dev>
2025-10-30 05:41:02 +0100

    minor: cleanups for PR ba40883f

--- internal/providers/desktopapplications/hyprland.go
+++ internal/providers/desktopapplications/hyprland.go
@@ -13,52 +13,53 @@ import (
 type Hyprland struct{}
 
 func (Hyprland) GetWorkspace() string {
-
 	cmd := exec.Command("hyprctl", "activeworkspace")
+
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		slog.Error(Name, "hyprlandworkspaces", err)
 		return ""
 	}
-	workspaceID := strings.Split(string(out), " ")[2]
+
+	workspaceID := strings.Fields(string(out))[2]
+
 	return workspaceID
 }
 
 func (c Hyprland) MoveToWorkspace(workspace, initialWMClass string) {
-	if initialWMClass == "" {
-		slog.Error(Name, "movetoworkspace", ".desktopfile has no StartupWMClass")
+	runtimeDir := os.Getenv("XDG_RUNTIME_DIR")
+	instanceSignature := os.Getenv("HYPRLAND_INSTANCE_SIGNATURE")
+
+	if runtimeDir == "" || instanceSignature == "" {
+		slog.Error(Name, "hyprlandmovetoworkspace", "XDG_RUNTIME_DIR or HYPRLAND_INSTANCE_SIGNATURE missing")
+
 		return
-
-	}
-	xdgRuntimeDir := os.Getenv("XDG_RUNTIME_DIR")
-	hyperlandInstanceSignature := os.Getenv("HYPRLAND_INSTANCE_SIGNATURE")
-	if xdgRuntimeDir == "" || hyperlandInstanceSignature == "" {
-		panic("xdgRuntimeDir is Null or hyprland is not running!")
 	}
 
-	socketpath := fmt.Sprintf("%s/hypr/%s/.socket2.sock", xdgRuntimeDir, hyperlandInstanceSignature)
+	socket := fmt.Sprintf("%s/hypr/%s/.socket2.sock", runtimeDir, instanceSignature)
 
-	conn, err := net.Dial("unix", socketpath)
+	conn, err := net.Dial("unix", socket)
 	if err != nil {
 		slog.Error(Name, "unix socket", err)
 	}
 	defer conn.Close()
+
 	scanner := bufio.NewScanner(conn)
 
 	for scanner.Scan() {
+		event := scanner.Text()
 
-		event := string(scanner.Bytes())
 		if strings.HasPrefix(event, "openwindow>>") {
 			windowinfo := strings.Split(strings.Split(event, ">>")[1], ",")
+
 			if windowinfo[2] == initialWMClass && windowinfo[1] != workspace {
-				bashline := fmt.Sprintf("hyprctl dispatch movetoworkspacesilent %s,address:0x%s", workspace, windowinfo[0])
-				slog.Info(Name, "movetoworkspace", bashline)
-				cmd := exec.Command("/usr/bin/env", "bash", "-c", bashline)
+				cmd := exec.Command("sh", "-c", fmt.Sprintf("hyprctl dispatch movetoworkspacesilent %s,address:0x%s", workspace, windowinfo[0]))
+
 				out, err := cmd.CombinedOutput()
 				if err != nil {
 					slog.Error(Name, "movetoworkspace", out)
 				}
-				slog.Info(Name, "movetoworkspace", string(out))
+
 				return
 			}
 		}
@@ -68,5 +69,4 @@ func (c Hyprland) MoveToWorkspace(workspace, initialWMClass string) {
 		slog.Error(Name, "monitor", err)
 		return
 	}
-
 }
